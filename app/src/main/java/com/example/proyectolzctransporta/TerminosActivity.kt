d4f87634997ca9ddf49082fac57e16684e473ee5
MiguelOlea60 <miguelolea60@gmail.com>
2023-09-19 17:59:40 -0600

    Ava

--- app/src/main/java/com/example/proyectolzctransporta/TerminosActivity.kt
+++ app/src/main/java/com/example/proyectolzctransporta/TerminosActivity.kt
@@ -1,8 +1,6 @@
 package com.example.proyectolzctransporta
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,32 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat.startActivity
 import com.example.proyectolzctransporta.ui.theme.ProyectoLZCTRANSPORTATheme
 
-class MainActivity : ComponentActivity() {
+class TerminosActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContent {R.layout}
     }
-
-
-
-
-    fun registrarme(v: View){
-        val intent = Intent(this, RegistrarmeActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun olvideContraseña(v: View){
-        startActivity(Intent(this,OlvideContraseñaActivity::class.java))
-    }
-
-    fun irTerminos(v: View){
-        val intent = Intent(this, TerminosActivity::class.java)
-        startActivity(intent)
-    }
-
 }
+
 
 
