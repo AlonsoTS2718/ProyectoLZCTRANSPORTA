9953598be626c212b2d1b8822bef0b37280ad945
MiguelOlea60 <miguelolea60@gmail.com>
2023-10-13 11:03:01 -0600

    Ayudaaaaaa

--- app/src/main/java/com/example/proyectolzctransporta/MapActivity.kt
+++ app/src/main/java/com/example/proyectolzctransporta/MapActivity.kt
@@ -3,34 +3,25 @@ package com.example.proyectolzctransporta
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.proyectolzctransporta.ui.theme.ProyectoLZCTRANSPORTATheme
+import androidx.appcompat.app.AppCompatActivity
+import com.example.proyectolzctransporta.R.*
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import androidx.fragment.app.supportFragmentManager
+import com.google.android.gms.maps.SupportMapFragment
 
 
-
-
-
-
-class MapActivity : ComponentActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
     private var googleMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.mapa)
+
+       setContentView(layout.mapa)
+
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+       val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
