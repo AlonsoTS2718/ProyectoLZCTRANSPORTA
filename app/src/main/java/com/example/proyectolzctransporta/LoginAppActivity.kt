de4baf56631e540c713cf18243fcff253a8c47b4
MiguelOlea60 <miguelolea60@gmail.com>
2023-10-13 22:32:24 -0600

    Error al entrar al mapa se sale de la app

--- app/src/main/java/com/example/proyectolzctransporta/LoginAppActivity.kt
+++ app/src/main/java/com/example/proyectolzctransporta/LoginAppActivity.kt
@@ -2,9 +2,10 @@ package com.example.proyectolzctransporta
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
-import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -37,29 +38,52 @@ class LoginAppActivity : ComponentActivity() {
         etInicioContrasena = findViewById(R.id.etInicioContrasena)
         mAuth = FirebaseAuth.getInstance()
 
+        val btnInicioSesion = findViewById<TextView>(R.id.btnInicioSesion)
+        btnInicioSesion.setOnClickListener {
+            login() // Llama al método login cuando se haga clic en el botón.
+        }
+        val txtInicioRegistrarme = findViewById<TextView>(R.id.txtInicioRegistrarme)
+        txtInicioRegistrarme.setOnClickListener {
+            registrar() // Llama al método login cuando se haga clic en el botón.
+        }
+
+
+
     }
-    fun login(view: View){
+    fun login() {
+        Log.d("LoginAppActivity", "Iniciando sesión")
         loginUser()
     }
     private fun loginUser() {
-        email = etInicioCorreo.text.toString();
+        email = etInicioCorreo.text.toString()
         password = etInicioContrasena.text.toString()
-        mAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    goMain(email, "email")
+        try {
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        goMain(email, "email")
+                    } else {
+                        // Manejar errores aquí y mostrar un mensaje al usuario
+                        val errorMessage = task.exception?.message ?: "Error desconocido"
+                        Toast.makeText(this, "Error al iniciar sesión: $errorMessage", Toast.LENGTH_SHORT).show()
+                    }
                 }
-            }
-
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("LoginAppActivity", "Error inesperado: ${e.message}")
+            Toast.makeText(this, "Error inesperado: ${e.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 
+
     private fun goMain(email: String, provider: String) {
+        Log.d("LoginAppActivity", "Ingresando a la actividad principal")
         useremail = email
         providerSession = provider
         startActivity(Intent(this, MapActivity::class.java))
     }
 
-    fun registrar(view: View){
+    fun registrar(){
         startActivity(Intent(this, RegistrarActivity::class.java))
     }
 
